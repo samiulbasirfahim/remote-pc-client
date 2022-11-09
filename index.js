f3034558a0994256ef3daa524561fd7c32898d42
samiul basir fahim <samiulbasirfahim360@gmail.com>
2022-11-09 18:49:21 +0600

    authantication done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const connection_info = document.getElementById("connection_info")
 const connection_panel = document.getElementById("connection_panel")
 const full_ui = document.getElementById("full_ui")
+const address_element = document.getElementById("address")
+const secret_key_element = document.getElementById("secret_key")
+const connect_button = document.getElementById("connect_button")
+
+const logout = document.getElementById("logout")
 
 const savedLoginInfo = JSON.parse(
   window.localStorage.getItem("remote-pc-controller")
@@ -10,8 +15,13 @@ const hide_element = (element) => {
   element.style.display = "none"
 }
 
-const connect = (ip, secret) => {
-  fetch(`http://${ip}`, {
+const unhide_element = (element) => {
+  element.style.display = "block"
+}
+
+const connect = (address, secret) => {
+  console.log(address, secret)
+  fetch(`http://${address}`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: secret,
@@ -21,6 +31,15 @@ const connect = (ip, secret) => {
     .then((data) => {
       console.log(data)
       if (data.Status) {
+        window.localStorage.setItem(
+          "remote-pc-controller",
+          JSON.stringify({
+            address: address,
+            secret_key: secret,
+          })
+        )
+        unhide_element(connection_info)
+        unhide_element(full_ui)
         return hide_element(connection_panel)
       } else {
         window.localStorage.removeItem("remote-pc-controller")
@@ -37,4 +56,15 @@ if (savedLoginInfo) {
 if (!savedLoginInfo) {
   hide_element(full_ui)
   hide_element(connection_info)
-} 
+}
+
+connect_button.addEventListener("click", () =>
+  connect(address_element.value, secret_key_element.value)
+)
+
+logout.addEventListener("click", () => {
+  window.localStorage.removeItem("remote-pc-controller")
+  unhide_element(connection_panel)
+  hide_element(full_ui)
+  hide_element(connection_info)
+})
