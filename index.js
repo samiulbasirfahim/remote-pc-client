6d9b35532dc2b42bb5910018f0c2f96877655073
samiul basir fahim <samiulbasirfahim360@gmail.com>
2022-11-09 18:20:45 +0600

    login from saved info done

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ const savedLoginInfo = JSON.parse(
   window.localStorage.getItem("remote-pc-controller")
 )
 
+const hide_element = (element) => {
+  element.style.display = "none"
+}
+
+const connect = (ip, secret) => {
+  fetch(`http://${ip}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: secret,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      if (data.Status) {
+        return hide_element(connection_panel)
+      } else {
+        window.localStorage.removeItem("remote-pc-controller")
+        hide_element(full_ui)
+        return hide_element(connection_info)
+      }
+    })
+}
+
+if (savedLoginInfo) {
+  connect(savedLoginInfo.address, savedLoginInfo.secret_key)
+}
+
+if (!savedLoginInfo) {
+  hide_element(full_ui)
+  hide_element(connection_info)
+} 
